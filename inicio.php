c2f118f5b8cd311058c0e3669e0bf07cc4432f49
Felirey03 <111838803+Felirey03@users.noreply.github.com>
2022-08-24 20:49:27 -0300

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="bootstrap-4.0.0\dist\css\bootstrap.min.css">
+<link href="bootstrap-5.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="bootstrap-5.2.0-dist/js/bootstrap.bundle.min.js"></script>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" type="text/css" href="conexion.php"/>
+</head>
+<body>
+   
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="inicio.php">Reyzarra</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="inicio.php">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="productos.php">Productos</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
+  </div>
+</nav>
+    </div>
+  </div>
+</body>
+</html>
