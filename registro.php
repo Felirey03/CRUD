fabe1e86cefbe54536160fa5cd8ad1231e6f8113
Felirey03 <111838803+Felirey03@users.noreply.github.com>
2022-08-24 20:47:39 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,5 +1,19 @@
 <?php
-include('conexion.php')
+
+
+include ('conexion.php');
+$cliente = $_POST["nombre"];
+$telefono = $_POST["telefono"];
+$email = $_POST["email"];
+$sql = $conn->prepare("INSERT INTO clientes(apynom,telefono,mail) VALUES ('$cliente','$telefono','$email')");
+$sql->execute();    
+
+
+//while($sql==true){
+//redireccion
+    header("Location: http://localhost/vinilos/pedido.php");
+    exit();
+//
 
 
 
