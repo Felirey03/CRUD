13166924c1e9cfd2d1785ca353dd29bb8c81b29a
Felirey03 <111838803+Felirey03@users.noreply.github.com>
2022-08-30 11:48:40 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -5,15 +5,15 @@ include ('conexion.php');
 $cliente = $_POST["nombre"];
 $telefono = $_POST["telefono"];
 $email = $_POST["email"];
+$tipo_producto = $_POST["producto"];
 $sql = $conn->prepare("INSERT INTO clientes(apynom,telefono,mail) VALUES ('$cliente','$telefono','$email')");
-$sql->execute();    
+$sql2 = $conn->prepare("INSERT INTO productos(tipo) VALUES ('$tipo_producto')");
+$sql->execute();
+$sql2->execute();      
 
+header("Location: http://localhost/vinilos/pedido.php");
+exit();
 
-//while($sql==true){
-//redireccion
-    header("Location: http://localhost/vinilos/pedido.php");
-    exit();
-//
 
 
 
